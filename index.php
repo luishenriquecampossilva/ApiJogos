b68ce0f7734ea9847d1184b98e8187d84183e491
adriano <adriano20fatec@gmail.com>
2020-05-21 20:59:23 -0300

    subindo arquivos

--- index.php
+++ index.php
@@ -84,7 +84,26 @@
   <body>
 
  
-  
+  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">RAWG</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lançementos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dowloads</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav> -->
    <div class = "seila">
     <h1 class = "titulo">RAWG<BR><span style = "color:#E8E2C1;font-family:arial;font-size:30px;font-style:italic;">GAMES</span></h1>
    
